04868a0790832daf002931eb198a15e3f5b3e2b4
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-15 15:31:03 +0800

    Update pg-demo.sql

--- pg-demo.sql
+++ pg-demo.sql
@@ -11,3 +11,17 @@ INSERT INTO test_table (name) VALUES ('yyy');
 SELECT * FROM test_table;
 
 drop database testdb;
+
+
+
+
+CREATE TABLE my_table (id int PRIMARY KEY, name VARCHAR(50));
+
+-- Example: Insert a lot of rows into a table
+BEGIN;
+INSERT INTO my_table (id, name) 
+SELECT generate_series(1, 10), md5(random()::text);
+COMMIT;
+
+
+
